cdba1f5bb7378c8b7fa584fbeb1c929368c4d8ff
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2025-01-29 13:36:16 +1000

    Added support for [] ranges and \ escapes

--- Match.c
+++ Match.c
@@ -25,7 +25,8 @@ enum
 };
 
 struct state {
-    int Symbol;
+    int SymbolRangeStart;
+    int SymbolRangeEnd;
     State* Transition1;  
     State* Transition2;
     // For checking StateSetList membership
@@ -86,14 +87,19 @@ void freeVisited() {
 }
 
 
-State* CreateState(int Symbol, State* Transition1, State* Transition2) {
+State* CreateState(int SymbolRangeStart, int SymbolRangeEnd, State* Transition1, State* Transition2) {
     State* NewState = malloc(sizeof(State));
-    printf("Created state -> %p\n", (void*)NewState);
-    NewState->Symbol = Symbol;
+    //printf("Created state -> %p\n", (void*)NewState);
+    NewState->SymbolRangeStart = SymbolRangeStart;
+    NewState->SymbolRangeEnd = SymbolRangeEnd;
     NewState->Transition1 = Transition1;
     NewState->Transition2 = Transition2;
     NewState->Mark = 0; 
     return NewState;
+}
+
+bool CheckStateOut(int Symbol, State* State) {
+    return (Symbol >= State->SymbolRangeStart && Symbol <= State->SymbolRangeEnd);
 }
 
 SubExpression CreateSubExpression(State* Start, PointerList* OutPointers)
@@ -118,7 +124,7 @@ void ConnectAutomata(PointerList* OutPointers, State* Start)
 PointerList* AppendOutPointers(PointerList* OutPointers, PointerList* MoreOutPointers)
 {
     if (OutPointers == NULL) {
-        printf("Appending NULL -> %p\n", (void*)MoreOutPointers);
+        //printf("Appending NULL -> %p\n", (void*)MoreOutPointers);
         return MoreOutPointers;
     }
 	PointerList* NewOutPointers = OutPointers;
@@ -127,21 +133,21 @@ PointerList* AppendOutPointers(PointerList* OutPointers, PointerList* MoreOutPoi
         OutPointers = OutPointers->NextPointer;
     }
     OutPointers->NextPointer = MoreOutPointers;
-    printf("Appending %p -> %p\n", (void*)OutPointers, (void*)MoreOutPointers);
+    //printf("Appending %p -> %p\n", (void*)OutPointers, (void*)MoreOutPointers);
     return NewOutPointers;
 }
 
 void FreePointerList(PointerList* list) {
     while (list) {
         PointerList* next = list->NextPointer;
-        printf("Freeing PointerList at %p\n", (void*)list);
+        //printf("Freeing PointerList at %p\n", (void*)list);
         free(list);
         list = next;
     }
 }
 
 SubExpression ApplyKleeneStar(SubExpression* NFA) {
-    State* NewStart = CreateState(Split, NFA->Start, NULL);
+    State* NewStart = CreateState(Split, Split, NFA->Start, NULL);
     ConnectAutomata(NFA->OutPointers, NewStart);
     FreePointerList(NFA->OutPointers);
     // Make a new PointerList with just NewStart->Transition2
@@ -152,7 +158,7 @@ SubExpression ApplyKleeneStar(SubExpression* NFA) {
 }
 
 SubExpression ApplyOneOrMore(SubExpression* NFA) {
-    State* NewSplit = CreateState(Split, NFA->Start, NULL);
+    State* NewSplit = CreateState(Split, Split, NFA->Start, NULL);
     ConnectAutomata(NFA->OutPointers, NewSplit);
     FreePointerList(NFA->OutPointers);
     // Make a new PointerList with just NewSplit->Transition2
@@ -163,7 +169,7 @@ SubExpression ApplyOneOrMore(SubExpression* NFA) {
 }
 
 SubExpression ApplyZeroOrOne(SubExpression* NFA) {
-    State* NewStart = CreateState(Split, NFA->Start, NULL);
+    State* NewStart = CreateState(Split, Split, NFA->Start, NULL);
     PointerList* NewOutPointer = malloc(sizeof(PointerList));
     NewOutPointer->CurrentPointer = &(NewStart->Transition2);
     NewOutPointer->NextPointer = NULL;
@@ -171,7 +177,7 @@ SubExpression ApplyZeroOrOne(SubExpression* NFA) {
 }
 
 SubExpression ApplyUnion(SubExpression* NFA1, SubExpression* NFA2) {
-    State* NewStart = CreateState(Split, NFA1->Start, NFA2->Start);
+    State* NewStart = CreateState(Split, Split, NFA1->Start, NFA2->Start);
     return CreateSubExpression(NewStart, AppendOutPointers(NFA1->OutPointers, NFA2->OutPointers));
 }
 
@@ -182,18 +188,42 @@ SubExpression ApplyConcatenation(SubExpression* NFA1, SubExpression* NFA2) {
 }
 
 SubExpression CreateSingleCharacter(int Symbol) {
-    State* Start = CreateState(Symbol,NULL,NULL);
+    State* Start = CreateState(Symbol, Symbol, NULL, NULL);
     PointerList* OutPointer = malloc(sizeof(PointerList));
-    printf("Allocated PointerList at %p\n", (void*)OutPointer);
+    //printf("Allocated PointerList at %p\n", (void*)OutPointer);
     OutPointer->CurrentPointer = &(Start->Transition1);
     OutPointer->NextPointer = NULL;
     return CreateSubExpression(Start, OutPointer);
+}
+
+SubExpression CreateRange(int StartSymbol, int EndSymbol) {
+    State* Start = CreateState(StartSymbol, EndSymbol, NULL, NULL);
+    PointerList* OutPointer = malloc(sizeof(PointerList));
+    //printf("Allocated PointerList at %p\n", (void*)OutPointer);
+    OutPointer->CurrentPointer = &(Start->Transition1);
+    OutPointer->NextPointer = NULL;
+    return CreateSubExpression(Start, OutPointer);
+}
+
+bool ParseCharacterRange(char** S, int* SymbolRange) {
+    // Check if we are a valid character and 
+    // if the next character is '-'
+    if(!(**S >= 32 && **S <= 255 && *(*S+1) == '-')) return false;
+    SymbolRange[0] = **S;
+    *S+=2;
+    // Check if we are a valid character and 
+    // if the next character is ']'
+    if(!(**S >= 32 && **S <= 255 && *(*S+1) == ']')) return false;
+    SymbolRange[1] = **S;
+    *S+=1;
+    return true;
 }
 
 SubExpression GenerateNFA(char* Regex) {
     SubExpression Stack[1000];
     SubExpression* StackPointer = Stack;
     SubExpression E1, E2, E;
+    int SymbolRange[2]  = {0, 0};
     char* Symbol = Regex;
 
     #define push(s) (*StackPointer++ = s)
@@ -230,6 +260,29 @@ SubExpression GenerateNFA(char* Regex) {
             E = pop();
             E = ApplyZeroOrOne(&E);
             push(E);
+        } else if (*Symbol == '\\') {
+            Symbol++;
+            if (*Symbol >= 32 && *Symbol <= 255) {
+                // Any printable ASCII character should 
+                // become a single state
+                E = CreateSingleCharacter(*Symbol);
+                push(E);
+            } else {
+                // Weird symbol
+                fprintf(stderr, "Invalid symbol '%c' in regex.\n", *Symbol);
+                exit(1);
+            }
+        } else if (*Symbol == '[') {
+            Symbol++;
+            // Ensure pattern matches "(start)-(end)]"
+            if (ParseCharacterRange(&Symbol, SymbolRange)) {
+                //fprintf(stderr, "At %c \n", *Symbol);
+                E = CreateRange(SymbolRange[0], SymbolRange[1]);
+                push(E);
+            } else {
+                fprintf(stderr, "Character range was specified incorrectly.");
+                exit(1);
+            }
         } else if (*Symbol >= 32 && *Symbol <= 255) {
             // Any printable ASCII character should 
             // become a single state
@@ -251,7 +304,7 @@ SubExpression GenerateNFA(char* Regex) {
     }
 
     E = pop();
-    State* MatchState = CreateState(Match, NULL, NULL);
+    State* MatchState = CreateState(Match, Match, NULL, NULL);
     ConnectAutomata(E.OutPointers, MatchState);
     // return pop();
     return E;
@@ -263,7 +316,7 @@ void Add(StateSetList* Set, State* S) {
     }
     S->Mark = Generation;
     //Ensure you do $\varepsilon$-closure
-    if(S->Symbol == Split) {
+    if(CheckStateOut(Split,S)) {
         Add(Set, S->Transition1);
         Add(Set, S->Transition2);
         return;
@@ -274,7 +327,7 @@ void Add(StateSetList* Set, State* S) {
 
 int SetContainsMatch(StateSetList* Set) {
     for(int i = 0; i < Set->Size; i++) {
-        if((Set->States[i])->Symbol == Match) {
+        if(CheckStateOut(Match, Set->States[i])) {
             return 1;
         }
     }
@@ -287,7 +340,7 @@ void StepThroughNFA(StateSetList* CurrentSet, StateSetList* NextSet, int Symbol)
     NextSet->Size = 0;
     for(int i = 0; i < CurrentSet->Size; i++) {
         S = CurrentSet->States[i];
-        if(S->Symbol == Symbol) {
+        if(CheckStateOut(Symbol,S)) {
             // Will never have Split states as Add 
             // always ensures that $\varepsilon$-closure is done
             Add(NextSet, S->Transition1);
@@ -321,7 +374,7 @@ void FreeState(State* state) {
     markVisited(state);
     FreeState(state->Transition1);
     FreeState(state->Transition2);
-    printf("Freed state -> %p\n", (void*)state);
+    //printf("Freed state -> %p\n", (void*)state);
     free(state);
 
 }
